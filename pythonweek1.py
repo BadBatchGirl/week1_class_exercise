3a3ce1436d4696466ad62fce948de33f0c06330f
Tamika Webb <94010537+BadBatchGirl@users.noreply.github.com>
2021-11-09 13:55:06 -0500

    Week 1 Reference

---
+++ pythonweek1.py
@@ -0,0 +1,21 @@
+#Exercise 1
+#1. Create a variable to hold a first name
+#2. Create a variable to hold the current year
+#3. Create a variable to hold the current price of gas
+#4. Create a variable to hold your favorite quote
+
+first_name = "Tamika"
+last_name = "Webb"
+current_year = 2021
+gas_price = "$0.88"
+favorite_quote = "Do as I say, not as I do ~ "
+
+#Exercise 2 Printing To The Console
+#Print the following message to the console: “Keep up the good work!”
+
+#Exercise 3 - Combining & Formatting Strings 
+# This is concatenating the literal string Norah to the value in message and printing it
+#print(f"{favorite_quote} Mama, {current_year}. Keep up the good work!")
+
+#Formatting string
+print(f"Hello {first_name} {last_name} it is nice to meet you!")
